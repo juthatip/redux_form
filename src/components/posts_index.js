e6c4e767d7f388ce542c437730e6eddfde730210
oh <w.juthatip@gmail.com>
2017-05-11 17:52:15 +0700

    update

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
+import { Link } from 'react-router-dom'
 
 class PostsIndex extends Component {
   componentDidMount() {
@@ -11,7 +12,9 @@ class PostsIndex extends Component {
   renderPosts() {
    return _.map(this.props.posts, post => {
      return (
-       <li className="list-group-item">{post.title}</li>
+       <li className="list-group-item" key={post.id}>
+         <Link to={`/posts/${post.id}`}> {post.title} </Link>
+       </li>
      )
    })
   }
@@ -19,6 +22,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right"> 
+          <Link className="btn btn-primary" to="/posts/new">
+          Add a Posts
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,89 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { createPost } from '../actions/index'
 
 class PostsNew extends Component {
-  render() {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
     return (
-      <div>
-        test
+      <div className={className}>
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input} 
+        />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
+    )
+  }
+
+  onSubmit(values) {
+    // this === component
+    // console.log(values)
+    this.props.createPost(values, () => {
+      this.props.history.push('/')
+    })
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+          />
+           <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+          />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField}
+          />
+          <button type="submit" className="btn btn-primary">Submit</button>
+          <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
     )
   }
 }
 
-export default PostsNew
+function validate(values) {
+  // console.log(values) -> { title: 'dddd', categories: 'sdfdfsds', content: 'dfdfdf'}
+  const errors = {};
+
+  // Validate the input from 'values'
+  if(!values.title) {
+    errors.title = 'Enter a title!';
+  }
+
+  if(!values.categories) {
+    errors.categories = 'enter some categories'
+  }
+
+  if(!values.content) {
+    errors.content = 'Enter some content please'
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If erros has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(
+  connect(null, { createPost })(PostsNew)
+);
+
